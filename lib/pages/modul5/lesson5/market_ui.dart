a0abda9a9974bb85e75aaaffbcca239e6907ac8e
askaroff939 <fayzullo.asqarov@gmail.com>
2022-06-28 15:53:01 +0500

    lesson 5/5

---
+++ lib/pages/modul5/lesson5/market_ui.dart
@@ -0,0 +1,195 @@
+import 'package:flutter/material.dart';
+
+class MarketUI extends StatefulWidget {
+  static const id = "/market_ui";
+
+  const MarketUI({Key? key}) : super(key: key);
+
+  @override
+  State<MarketUI> createState() => _MarketUIState();
+}
+
+class _MarketUIState extends State<MarketUI> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        centerTitle: false,
+        elevation: 0,
+        backgroundColor: Colors.white,
+        title: const Text(
+          "Cars",
+          style: TextStyle(
+            color: Colors.red,
+            fontSize: 25,
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.notifications_none,
+              color: Colors.red,
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.shopping_cart,
+              color: Colors.red,
+            ),
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            children: [
+              Container(
+                height: 40,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    singleTab(true, "All"),
+                    singleTab(false, "Red"),
+                    singleTab(false, "Blue"),
+                    singleTab(false, "Green"),
+                    singleTab(false, "Yellow"),
+                    singleTab(false, "Black"),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              makeItem("assets/images/img_12.png"),
+              makeItem("assets/images/img_13.png"),
+              makeItem("assets/images/img_14.png"),
+              makeItem("assets/images/img_15.png"),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget singleTab(bool type, String txt) {
+    return AspectRatio(
+      aspectRatio: 2.2 / 1,
+      child: Container(
+        margin: const EdgeInsets.only(right: 10),
+        decoration: BoxDecoration(
+          color: type ? Colors.red : Colors.white,
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Center(
+          child: Text(
+            txt,
+            style: TextStyle(
+              color: type ? Colors.white : Colors.black,
+              fontSize: type ? 20 : 17,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget makeItem(String image) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10),
+      height: 250,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        image: DecorationImage(
+          image: AssetImage(image),
+          fit: BoxFit.cover,
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.shade400,
+            blurRadius: 10,
+            offset: const Offset(0, 10),
+          ),
+        ],
+      ),
+      child: Container(
+        padding: const EdgeInsets.all(20),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            gradient: LinearGradient(
+              begin: Alignment.bottomRight,
+              colors: [
+                Colors.black.withOpacity(0.9),
+                Colors.black.withOpacity(0.6),
+                Colors.black.withOpacity(0.3),
+                Colors.black.withOpacity(0.2),
+              ],
+            )),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Expanded(
+              flex: 3,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: const [
+                      Text(
+                        "PDP Car",
+                        style: TextStyle(
+                          fontSize: 25,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600
+                        ),
+                      ),
+                      SizedBox(width: 10,),
+                      Text(
+                        "Electric",
+                        style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.red,
+                          fontWeight: FontWeight.w600
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 10,),
+                  const Text(
+                    "100\$",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 25,
+                        fontWeight: FontWeight.w600
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            Expanded(
+              child: Container(
+                width: 35,
+                height: 35,
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.red,
+                ),
+                child: const Icon(
+                  Icons.favorite_border,
+                  size: 20,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
